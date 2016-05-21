e16645a9178b7a5e138adb965805bf2e5e9cd95b
Jan Rabe <cloudgazer3d@gmail.com>
2016-05-21 11:49:29 +0200

    added a scale drawable method

--- common-android-utils/src/main/java/com/common/android/utils/extensions/DrawableExtensions.java
+++ common-android-utils/src/main/java/com/common/android/utils/extensions/DrawableExtensions.java
@@ -1,7 +1,33 @@
 package com.common.android.utils.extensions;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+
+import static com.common.android.utils.ContextHelper.getContext;
+
 /**
  * Created by Nyaruhodo on 21.05.2016.
  */
 public class DrawableExtensions {
+    @Nullable
+    public static Drawable scaleImage(@Nullable Drawable image, float scaleFactor) {
+
+        if ((image == null) || !(image instanceof BitmapDrawable)) {
+            return image;
+        }
+
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+
+        int sizeX = Math.round(image.getIntrinsicWidth() * scaleFactor);
+        int sizeY = Math.round(image.getIntrinsicHeight() * scaleFactor);
+
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, sizeX, sizeY, false);
+
+        image = new BitmapDrawable(getContext().getResources(), bitmapResized);
+
+        return image;
+
+    }
 }
