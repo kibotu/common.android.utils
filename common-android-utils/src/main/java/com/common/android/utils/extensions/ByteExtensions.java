062123897bfa986093850bcd50efc592be5e23cc
Jan Rabe <jan.rabe@exozet.com>
2016-08-10 14:17:02 +0200

    added method to fix ut8 encoding in a string on byte level

---
+++ common-android-utils/src/main/java/com/common/android/utils/extensions/ByteExtensions.java
@@ -0,0 +1,59 @@
+package com.common.android.utils.extensions;
+
+/**
+ * Created by jan.rabe on 10/08/16.
+ */
+
+public class ByteExtensions {
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static boolean validUTF8(byte[] input) {
+        int i = 0;
+        // Check for BOM
+        if (input.length >= 3 && (input[0] & 0xFF) == 0xEF
+                && (input[1] & 0xFF) == 0xBB & (input[2] & 0xFF) == 0xBF) {
+            i = 3;
+        }
+
+        int end;
+        for (int j = input.length; i < j; ++i) {
+            int octet = input[i];
+            if ((octet & 0x80) == 0) {
+                continue; // ASCII
+            }
+
+            // Check for UTF-8 leading byte
+            if ((octet & 0xE0) == 0xC0) {
+                end = i + 1;
+            } else if ((octet & 0xF0) == 0xE0) {
+                end = i + 2;
+            } else if ((octet & 0xF8) == 0xF0) {
+                end = i + 3;
+            } else {
+                // Java only supports BMP so 3 is max
+                return false;
+            }
+
+            while (i < end) {
+                i++;
+                octet = input[i];
+                if ((octet & 0xC0) != 0x80) {
+                    // Not a valid trailing byte
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
--- common-android-utils/src/main/java/com/common/android/utils/extensions/StringExtensions.java
+++ common-android-utils/src/main/java/com/common/android/utils/extensions/StringExtensions.java
@@ -4,6 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+import java.io.UnsupportedEncodingException;
+
+import static com.common.android.utils.extensions.ByteExtensions.validUTF8;
+
 /**
  * Created by Jan Rabe on 14/10/15.
  */
@@ -19,5 +23,20 @@ final public class StringExtensions {
             return "";
 
         return s.substring(0, (int) MathExtensions.clamp(s.length(), 0, maxLength));
+    }
+
+    /**
+     * http://stackoverflow.com/a/1447720
+     */
+    public static String fixEncoding(String latin1) {
+        try {
+            byte[] bytes = latin1.getBytes("ISO-8859-1");
+            if (!validUTF8(bytes))
+                return latin1;
+            return new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // Impossible, throw unchecked
+            throw new IllegalStateException("No Latin1 or UTF-8: " + e.getMessage());
+        }
     }
 }
