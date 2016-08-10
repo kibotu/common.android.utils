135302569df674376dec378b4a9b316bb9cc9ac1
Jan Rabe <jan.rabe@exozet.com>
2016-08-10 14:16:11 +0200

    added global getString method using current context

--- common-android-utils/src/main/java/com/common/android/utils/extensions/ResourceExtensions.java
+++ common-android-utils/src/main/java/com/common/android/utils/extensions/ResourceExtensions.java
@@ -11,6 +11,7 @@ import android.support.annotation.DimenRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 
 import static android.os.Build.VERSION.SDK_INT;
@@ -24,6 +25,10 @@ final public class ResourceExtensions {
 
     private ResourceExtensions() throws IllegalAccessException {
         throw new IllegalAccessException();
+    }
+
+    public static String getString(@StringRes final int id) {
+        return getContext().getString(id);
     }
 
     public static int dimension(@DimenRes final int resId) {
